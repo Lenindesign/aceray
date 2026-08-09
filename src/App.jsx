1f0c1cb6d06ab143a9e67f888803d92ee2394e75
Lenin Aviles <Lenin_Aviles@hotmail.com>
2026-08-08 19:33:20 -0700

    feat: remove site password protection gate to unlock public site access

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { lazy, Suspense } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from '@/components/Layout'
 import ScrollToTop from '@/components/ScrollToTop'
-import SitePasswordGate from '@/components/SitePasswordGate'
 
 const HomePage = lazy(() => import('@/pages/HomePage'))
 const ProductPage = lazy(() => import('@/pages/ProductPage'))
@@ -22,35 +21,33 @@ const NotFoundPage = lazy(() => import('@/pages/NotFoundPage'))
 
 export default function App() {
   return (
-    <SitePasswordGate>
-      <BrowserRouter>
-        <ScrollToTop />
-        <Suspense fallback={<div className="route-loading" aria-live="polite">Loading...</div>}>
-          <Routes>
-            <Route element={<Layout />}>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/catalog" element={<CatalogPage />} />
-              <Route path="/blog" element={<BlogPage />} />
-              <Route path="/product" element={<ProductPage />} />
-              <Route path="/product/:slug" element={<ProductPage />} />
-              <Route path="/about" element={<AboutPage />} />
-              <Route path="/contact" element={<ContactPage />} />
-              <Route path="/collections" element={<CollectionsPage />} />
-              <Route path="/collections/:familySlug" element={<FamilyLandingPage />} />
-              <Route path="/designers" element={<DesignersPage />} />
-              <Route path="/designers/:designerSlug" element={<DesignerLandingPage />} />
-              <Route path="/installations" element={<InstallationsPage />} />
-              <Route path="/installation-gallery" element={<InstallationsPage />} />
-              <Route path="/gallery" element={<InstallationsPage />} />
-              <Route path="/projects" element={<InstallationsPage />} />
-              <Route path="/resources" element={<ResourcesPage />} />
-              <Route path="/fabrics-finishes" element={<FabricsFinishesPage />} />
-              <Route path="/aceray-book" element={<AcerayBookPage />} />
-              <Route path="*" element={<NotFoundPage />} />
-            </Route>
-          </Routes>
-        </Suspense>
-      </BrowserRouter>
-    </SitePasswordGate>
+    <BrowserRouter>
+      <ScrollToTop />
+      <Suspense fallback={<div className="route-loading" aria-live="polite">Loading...</div>}>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/catalog" element={<CatalogPage />} />
+            <Route path="/blog" element={<BlogPage />} />
+            <Route path="/product" element={<ProductPage />} />
+            <Route path="/product/:slug" element={<ProductPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/collections" element={<CollectionsPage />} />
+            <Route path="/collections/:familySlug" element={<FamilyLandingPage />} />
+            <Route path="/designers" element={<DesignersPage />} />
+            <Route path="/designers/:designerSlug" element={<DesignerLandingPage />} />
+            <Route path="/installations" element={<InstallationsPage />} />
+            <Route path="/installation-gallery" element={<InstallationsPage />} />
+            <Route path="/gallery" element={<InstallationsPage />} />
+            <Route path="/projects" element={<InstallationsPage />} />
+            <Route path="/resources" element={<ResourcesPage />} />
+            <Route path="/fabrics-finishes" element={<FabricsFinishesPage />} />
+            <Route path="/aceray-book" element={<AcerayBookPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   )
 }
