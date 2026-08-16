7f01708b3a2bed5fd41a206ad84f245759f35851
Lenin Aviles <Lenin_Aviles@hotmail.com>
2026-08-15 19:14:41 -0700

    fix(catalog): add missing useEffect and Link imports to AcerayBookPage and ResourcesPage to resolve blank page crash

--- src/pages/ResourcesPage.jsx
+++ src/pages/ResourcesPage.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import { removeSeoJsonLd, setSeoMetadata, createBreadcrumbJsonLd, ACERAY_ORGANIZATION_SCHEMA } from '@/lib/seo'
 
 const RESOURCES = [
